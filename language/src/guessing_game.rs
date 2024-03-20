050b5cda31e0a9699901747197c8fe3d39d2f2a1
HareemSaad <hareems20014@outlook.com>
2024-03-20 12:54:37 +0500

    trim and compare methods

--- language/src/guessing_game.rs
+++ language/src/guessing_game.rs
@@ -1,4 +1,5 @@
 use std::io; // standard input output library
+use std::cmp::Ordering;
 use rand::Rng; // Rng is a trait
 
 pub fn game() {
@@ -8,7 +9,7 @@ pub fn game() {
     // The gen_range method takes a range expression as an argument and generates a random number in the range. The kind of range expression we’re using here takes the form start..=end and is inclusive on the lower and upper bounds, so we need to specify 1..=100 to request a number between 1 and 100.
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -20,5 +21,16 @@ pub fn game() {
         // writing &mut allows us to modify `guess` variable (allows to use mutability)
         .expect("Failed to read line"); 
 
+    // The trim method on a String instance will eliminate any whitespace at the beginning and end
+    // The parse method on strings converts a string to another type. We need to tell Rust the exact number type we want by using let guess: u32
+    //  If parse returns an Err Result variant because it couldn’t create a number from the string, the expect call will crash the game and print the message we give it. If parse can successfully convert the string to a number, it will return the Ok variant of Result, and expect will return the number that we want from the Ok value.
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
