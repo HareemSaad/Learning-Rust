098d8e6308481c7f231a963c2fdec9b79ecc11e7
HareemSaad <hareems20014@outlook.com>
2024-03-20 23:00:38 +0500

    Loop Labels to Disambiguate Between Multiple Loops

--- language/src/common_concepts.rs
+++ language/src/common_concepts.rs
@@ -166,6 +166,27 @@ pub fn common_concepts() {
     };
 
     println!("The result is {result}");
+
+    // If you have loops within loops, break and continue apply to the innermost loop at that point. You can optionally specify a loop label on a loop that you can then use with break or continue to specify that those keywords apply to the labeled loop instead of the innermost loop. Loop labels must begin with a single quote. Here’s an example with two nested loops:
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
