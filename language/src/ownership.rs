feb2b60e9add38606d56271aa3d8a2fdbb3fe85d
HareemSaad <hareems20014@outlook.com>
2024-03-22 13:28:37 +0500

    literals vs string

--- language/src/ownership.rs
+++ language/src/ownership.rs
@@ -8,4 +8,17 @@ pub fn ownership() {
     }                      // this scope is now over, and s is no longer valid
     // println!("{}", s);     // this will NOT work
 
+    let mut s = String::from("hello");
+    // let s = "hello"; // this is a literal; literals cannot be mutated, strings can be
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+    // In the case of a string literal, we know the contents at compile time, so the text is hardcoded directly into the final executable. This is why string literals are fast and efficient.
+    // In the case of a string literal, we know the contents at compile time, so the text is hardcoded directly into the final executable. This is why string literals are fast and efficient.
+    //  - The memory must be requested from the memory allocator at runtime. (done when calling `String::from`)
+    //  - We need a way of returning this memory to the allocator when we’re done with our String. (done when variable goes out of scope)
+
+
 }
