3a044802164a2bfe4bfa26d5b1231a8acd76b280
HareemSaad <hareems20014@outlook.com>
2024-03-27 13:21:18 +0500

    binding val with Option<T>

--- language/src/match_control_flow.rs
+++ language/src/match_control_flow.rs
@@ -16,6 +16,12 @@ pub fn match_control_flow() {
     
     println!("{}", value_in_cents(Coin::Penny));
     println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -30,5 +36,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("State quarter from {:?}!", state);
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None, // rust will throw error if you forget none case, hence matc is exhaustive
+        Some(i) => Some(i + 1), // The i binds to the value contained in Some, so i takes the value 5. 
     }
 }
