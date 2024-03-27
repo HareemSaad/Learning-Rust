81deb03a8aed0f8ae53e65ac0da99b71ca7cc222
HareemSaad <hareems20014@outlook.com>
2024-03-27 13:24:28 +0500

    match syntax with 'default' case

--- language/src/match_control_flow.rs
+++ language/src/match_control_flow.rs
@@ -22,6 +22,19 @@ pub fn match_control_flow() {
     let none = plus_one(None);
     println!("{:?}", six);
     println!("{:?}", none);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other), // other binds to all values not 3 or 7 so all cases are handled
+    }
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(), // handles cases except 3 and 7 but does not bind to value
+        // _ => () if you want to call no function
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -45,3 +58,8 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
         Some(i) => Some(i + 1), // The i binds to the value contained in Some, so i takes the value 5. 
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {}
