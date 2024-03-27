f8ce2eaad2021134006f3836de7bd9dfd96bdaee
HareemSaad <hareems20014@outlook.com>
2024-03-27 11:33:01 +0500

    using debug trait to print and debug strcuts & code

--- language/src/structs_advanced.rs
+++ language/src/structs_advanced.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)] // trair so this struct can be printed using println!
 struct Rectangle {
     width: u32,
     height: u32,
@@ -10,11 +11,21 @@ pub fn structs_advanced() {
         height: 50,
     };
 
+    println!("rect1 is {:?}\n", rect1); // prints struct in single line
+    println!("rect1 is {:#?}\n", rect1); // prints struct in multiple lines with formatiing
+
+    dbg!(&rect1); // prints it with file and line number
+
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle is {} square pixels.\n",
         area(&rect1)
     );
-    
+
+    let scale = 9;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
