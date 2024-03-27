2770cbe1f3853940f2c2321a399cc73c80549e95
HareemSaad <hareems20014@outlook.com>
2024-03-27 12:34:21 +0500

    enums containing structs

--- language/src/enums.rs
+++ language/src/enums.rs
@@ -5,8 +5,51 @@ enum Shape {
     OVAL
 }
 
+#[derive(Debug)]
+enum Color {
+    GOLD,
+    SILVER,
+    BRONZE
+}
+
+#[derive(Debug)]
+struct Medal {
+    shape: Shape,
+    color: Color
+}
+
+const WINNER_MEDAL: Medal = Medal {
+    shape: Shape::CIRCLE,
+    color: Color::GOLD,
+};
+
+const RUNNER_UP_MEDAL: Medal = Medal {
+    shape: Shape::SQUARE,
+    color: Color::SILVER,
+};
+
+const SECOND_RUNNER_UP_MEDAL: Medal = Medal {
+    shape: Shape::OVAL,
+    color: Color::BRONZE,
+};
+
+#[derive(Debug)]
+enum Prize {
+    WINNER(Medal), // only type defination allowed in ()
+    RUNNER_UP(Medal),
+    SECOND_RUNNER_UP(Medal)
+}
+
 pub fn enums() {
     
     let circle = Shape::CIRCLE;
     println!("Circle is {:?}", circle);
+
+    let winner_prize = Prize::WINNER(WINNER_MEDAL);
+    let runner_up_prize = Prize::RUNNER_UP(RUNNER_UP_MEDAL);
+    let second_runner_up_prize = Prize::SECOND_RUNNER_UP(SECOND_RUNNER_UP_MEDAL);
+
+    println!("{:?}", winner_prize);
+    println!("{:?}", runner_up_prize);
+    println!("{:?}", second_runner_up_prize);
 }
