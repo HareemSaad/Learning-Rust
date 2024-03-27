887cda9469ffe7346d6894f16bd42613893561fd
HareemSaad <hareems20014@outlook.com>
2024-03-27 13:59:58 +0500

    if let control fllow where match takes owner ship of variable: copy trait, references

--- language/src/if_let_control_flow.rs
+++ language/src/if_let_control_flow.rs
@@ -25,20 +25,22 @@ pub fn if_let_control_flow() {
         println!("The maximum is configured to be {}", max);
     } 
 
+    // you cannot use the same coin variable for the 2 below code blocks, because match will take ownership of coin,
+    // you can either re initialize the val or use reference '&' or implement the copy trait by writing #[derive(Debug, Clone, Copy)] above both enums
     let coin: Coin = Coin::Quarter(UsState::Alabama);
     let mut count = 0;
-    match coin {
+    match &coin { // use reference
         Coin::Quarter(state) => println!("State quarter from {:?}!", state),
         _ => count += 1,
     }
     
     // or
     
-    // if let Coin::Quarter(state) = coin {
-    //     println!("State quarter from {:?}!", state);
-    // } else {
-    //     count += 1;
-    // }
-
+    // let coin: Coin = Coin::Quarter(UsState::Alabama); // reinitialize
+    if let Coin::Quarter(state) = &coin { // use refernce
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 
 }
