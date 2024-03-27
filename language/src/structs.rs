0f9fe60e4e5d1130fde6a95ae2d45c045aecc8e6
HareemSaad <hareems20014@outlook.com>
2024-03-27 11:06:44 +0500

    returning structs from functions

--- language/src/structs.rs
+++ language/src/structs.rs
@@ -6,14 +6,21 @@ struct User {
 }
 
 pub fn structs() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123")
+    );
     println!("{}",  user1.email);
     
     user1.email = String::from("anotheremail@example.com");
     println!("{}",  user1.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username, // since parameter names = struct field names we can just write `username.`
+        email: email, // since parameter names = struct field names we can just write `email.`
+        sign_in_count: 1,
+    }
 }
