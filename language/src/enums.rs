a7008ef55cf4a5790123fc9657eec122cc80a6fc
HareemSaad <hareems20014@outlook.com>
2024-03-27 13:01:45 +0500

    Option<T>

--- language/src/enums.rs
+++ language/src/enums.rs
@@ -59,4 +59,19 @@ pub fn enums() {
     println!("{:?}", winner_prize);
     println!("{:?}", runner_up_prize);
     println!("{:?}", second_runner_up_prize);
+
+    let some_number = Some(3); // enforces no nulls by having to declare what happens if a value is null
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None; // assign i32 null
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum = x + y; wont add because x is type i8 and y is Option<i8>: convert Option<i8> to i8
+    let x: Option<u32> = None;
+    let y: Option<u32> = Some(12);
+
+    assert_eq!(x.unwrap_or_default(), 0);
+    assert_eq!(y.unwrap_or_default(), 12);
 }
