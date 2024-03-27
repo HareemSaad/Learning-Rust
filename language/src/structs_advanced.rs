17ab8ecbd1fea3e967c9694bda4f13c1b981bd3d
HareemSaad <hareems20014@outlook.com>
2024-03-27 11:48:24 +0500

    method syntax

--- language/src/structs_advanced.rs
+++ language/src/structs_advanced.rs
@@ -1,7 +1,20 @@
-#[derive(Debug)] // trair so this struct can be printed using println!
+#[derive(Debug)] // trait so this struct can be printed using println!
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+// associated functions
+impl Rectangle { // impl struct_name: implementation block (contains all methods)
+    // Methods can take ownership of self, borrow self immutably, as we’ve done here, or borrow self mutably (&mut self), just as they can any other parameter.
+    fn area(&self) -> u32 { // method function &self (borrows the Self (calling) instance)
+        self.width * self.height
+    }
+
+    // other: &Rectangle: takes a reference to an existing rectangle
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 pub fn structs_advanced() {
@@ -18,16 +31,13 @@ pub fn structs_advanced() {
 
     println!(
         "The area of the rectangle is {} square pixels.\n",
-        area(&rect1)
+        rect1.area()
     );
-
+    
     let scale = 9;
     let rect2 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
