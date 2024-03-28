d5dbd8535bc2b397c0aa2216b72bb3d0cf9f218a
HareemSaad <hareems20014@outlook.com>
2024-03-28 11:50:01 +0500

    vector matching, get method

--- language/src/vectors.rs
+++ language/src/vectors.rs
@@ -12,4 +12,16 @@ pub fn vectors() {
     v.push(8);
 
     println!("{:?}", v); //print vector
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2); // get method gives us Option<T> value
+    let fourth: Option<&i32> = v.get(3);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
