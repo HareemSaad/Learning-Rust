36428e7ef52815f093b66e1a3a7032ea54960d86
HareemSaad <hareems20014@outlook.com>
2024-03-28 12:37:33 +0500

    getting from hashmaps, copied and unwrap or methods

--- language/src/hash_maps.rs
+++ language/src/hash_maps.rs
@@ -5,5 +5,8 @@ pub fn hash_maps() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
-
+    
+    // The get method returns an Option<&V>; if there’s no value for that key in the hash map, get will return None. This program handles the Option by calling copied to get an Option<i32> rather than an Option<&i32>, then unwrap_or to set score to zero if scores doesn't have an entry for the key.
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
 }
